00f26c1679c749bfc2b1983ca038c6451e37acea
unknown <abhi.pandravada@gmail.com>
2022-08-29 16:36:43 -0400

    Form validation added

--- src/components/AddTaskForm.js
+++ src/components/AddTaskForm.js
@@ -1,30 +1,53 @@
 import "./AddTaskForm.css";
-import {useState} from 'react';
+import ErrorModal from "./ErrorModal";
+import { useState } from "react";
 
 function AddTaskForm(props) {
+  const [newTask, setNewTask] = useState("");
+  const [error, setError] = useState();
 
-  const [newTask, setNewTask] = useState('');
-
-  const inputTaskChangeHandler = event => {
+  const inputTaskChangeHandler = (event) => {
     setNewTask(event.target.value);
-  }
+  };
 
   const onSubmitHandler = (event) => {
     event.preventDefault();
     const newTaskData = {
-      title : newTask,
-      isCompleted : false
+      title: newTask,
+      isCompleted: false,
+    };
+    console.log(newTask);
+    if (newTask.trim().length === 0) {
+      setError({
+        title: "No task entered",
+        message: "Please enter a valid input (must be non-empty)"
+      })
+      return;
     }
-    setNewTask('');
+    setNewTask("");
     props.onSaveTaskHandler(newTaskData);
+  };
+
+  const errorHandler = () => {
+    setError(null)
   }
-  
+
   return (
-    <div className='footer'>
-      <form onSubmit={onSubmitHandler} className='add-task-container'>
-        <input onChange={inputTaskChangeHandler} className='input-text-field' type="text" value={newTask} />
-        <button className='btn-add-task' type="submit">Add task</button>
-      </form>
+    <div>
+      {error &&<ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
+      <div className="footer">
+        <form onSubmit={onSubmitHandler} className="add-task-container">
+          <input
+            onChange={inputTaskChangeHandler}
+            className="input-text-field"
+            type="text"
+            value={newTask}
+          />
+          <button className="btn-add-task" type="submit">
+            Add task
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
